583825a416a51e183a36df66b351f271cc64a5e5
Muha Saad <mu7ammadsa3dd@gmail.com>
2023-11-19 23:27:34 +0200

    Initial commit

---
+++ src/TeamsMaker.Api/ServiceRegistrationExtensions.cs
@@ -0,0 +1,9 @@
+namespace TeamsMaker.Api;
+
+public static class ServiceRegistrationExtensions
+{
+    public static IServiceCollection RegisterBusinessServices(this IServiceCollection services)
+    {
+        return services;
+    }
+}
---
+++ test/TeamsMaker.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace TeamsMaker.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
