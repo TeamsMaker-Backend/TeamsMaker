12b68fe32d1159aaf9db0959c94d2301081ad743
Adham Khalid <6um6l6ee@gmail.com>
2024-03-11 16:25:41 +0200

    [Add] in IFileService - UpdateFile (Service), (Request), (Endpoint)

---
+++ src/TeamsMaker.Api/Controllers/Files/UpdateFileEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using TeamsMaker.Api.Contracts.Requests.File;
+using TeamsMaker.Api.Services.Files.Interfaces;
+
+namespace TeamsMaker.Api.Controllers.Files;
+
+[Authorize]
+public class UpdateFileEndpoint(IServiceProvider serviceProvider) : BaseApiController
+{
+    [Tags("files")]
+    [HttpPut("files/{baseType}/{id}/{fileType}")]
+    public async Task<IActionResult> File(string baseType, string id, string fileType, [FromForm] UpdateFileRequest request, CancellationToken ct)
+    {
+        var fileService = serviceProvider.GetRequiredKeyedService<IFileService>(baseType);
+
+        try
+        {
+            await fileService.UpdateFileAsync(id, fileType, request, ct);
+        }
+        catch (ArgumentException e)
+        {
+            return NotFound(_response.FailureResponse(e.Message));
+        }
+
+        return Ok();
+    }
+}
--- src/TeamsMaker.Api/Services/Files/Interfaces/IFileService.cs
+++ src/TeamsMaker.Api/Services/Files/Interfaces/IFileService.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using TeamsMaker.Api.Contracts.Requests.File;
+
 namespace TeamsMaker.Api.Services.Files.Interfaces;
 
 public interface IFileService
 {
+    Task UpdateFileAsync(string id, string fileType, UpdateFileRequest request, CancellationToken ct);
     Task<FileContentResult?> GetFileContentAsync(string id, string fileType, CancellationToken ct);
     string? GetFileUrl(string id, string fileType); //TODO: [Refactor] Remove dublicates Urgent
 }
