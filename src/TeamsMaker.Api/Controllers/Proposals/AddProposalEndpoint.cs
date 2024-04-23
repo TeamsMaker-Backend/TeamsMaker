c993a9d3dfa4488c357615e1ebacfc6f8d905570
Muha Saad <mu7ammadsa3dd@gmail.com>
2024-04-23 21:26:27 +0200

    [Add] add proposal endpoints

---
+++ src/TeamsMaker.Api/Controllers/Proposals/AddProposalEndpoint.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using TeamsMaker.Api.Contracts.Requests.Proposal;
+
+using TeamsMaker.Api.Contracts.Responses;
+using TeamsMaker.Api.Services.Proposals.Interfaces;
+
+namespace TeamsMaker.Api.Controllers.Proposals;
+
+[Authorize]
+public class AddProposalEndpoint(IProposalService proposalService) : BaseApiController
+{
+    [Tags("proposal")]
+    [Produces<IdResponse<Guid>>]
+    [HttpPost("proposal")]
+    public async Task<IActionResult> Proposal(AddProposalRequest request, CancellationToken ct)
+    {
+        Guid proposalId;
+
+        try
+        {
+            proposalId = await proposalService.AddAsync(request, ct);
+        }
+        catch (Exception e)
+        {
+            return NotFound(_response.FailureResponse(e.Message));
+        }
+
+        return Created("", _response.SuccessResponse(new IdResponse<Guid>(proposalId)));
+    }
+}
---
+++ src/TeamsMaker.Api/Controllers/Proposals/GetProposalEndpoint.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+using TeamsMaker.Api.Contracts.Responses.Proposal;
+using TeamsMaker.Api.Services.Proposals.Interfaces;
+
+namespace TeamsMaker.Api.Controllers.Proposals;
+
+[Authorize]
+public class GetProposalEndpoint(IProposalService proposalService) : BaseApiController
+{
+    [Tags("proposal")]
+    [Produces<GetProposalEndpoint>]
+    [HttpGet("proposal/{circleId}")]
+    public async Task<IActionResult> Proposal(Guid circleId, CancellationToken ct)
+    {
+        GetProposalResponse response;
+
+        try
+        {
+            response = await proposalService.GetAsync(circleId, ct);
+        }
+        catch (Exception e)
+        {
+            return NotFound(_response.FailureResponse(e.Message));
+        }
+
+        return Created("", _response.SuccessResponse(response));
+    }
+}
