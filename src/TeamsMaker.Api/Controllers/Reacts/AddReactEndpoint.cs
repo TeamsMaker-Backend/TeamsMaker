42914723f18f0f2181df6ae2c2fe4c591c2883e2
Abdelrahman Elghonemy <abdelrahmanel8nemy2020@outlook.com>
2024-05-12 02:06:57 +0300

    add reacts and delete reacts

---
+++ src/TeamsMaker.Api/Controllers/Reacts/AddReactEndpoint.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using TeamsMaker.Api.Contracts.Responses;
+using TeamsMaker.Api.Services.Reacts.Interfaces;
+
+namespace TeamsMaker.Api.Controllers.Reacts;
+
+[Authorize]
+public class AddReactEndpoint(IReactService reactService) : BaseApiController
+{
+    [Tags("React")]
+    [Produces<IdResponse<Guid>>]
+    [HttpPost("react")]
+    public async Task<IActionResult> AddReact ([FromQuery] Guid id, CancellationToken ct)
+    {
+        Guid reactId;
+        try
+        {
+            reactId = await reactService.AddAsync(id, ct);
+        }
+        catch (Exception e)
+        {
+            return NotFound(_response.FailureResponse(e.Message));
+        }
+        return Created("", _response.SuccessResponse(new IdResponse<Guid>(reactId)));
+    }
+}
