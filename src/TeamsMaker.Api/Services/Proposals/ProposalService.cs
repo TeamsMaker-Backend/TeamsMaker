ba80946b091adac49075001132dfa50ae618607f
Muha Saad <mu7ammadsa3dd@gmail.com>
2024-05-17 02:37:54 +0300

    [fix] fix circle member getting

--- src/TeamsMaker.Api/Services/Proposals/ProposalService.cs
+++ src/TeamsMaker.Api/Services/Proposals/ProposalService.cs
@@ -22,7 +22,7 @@ public class ProposalService(AppDBContext db, IUserInfo userInfo,
 
         GetProposalResponse? proposal = null;
 
-        if(circle.Proposal is not null)
+        if (circle.Proposal is not null)
             proposal = new GetProposalResponse
             {
                 Id = circle.Proposal!.Id,
@@ -97,7 +97,7 @@ public class ProposalService(AppDBContext db, IUserInfo userInfo,
     {
         var proposal = await db.Proposals
             .Include(p => p.Circle)
-                .ThenInclude(c => c.CircleMembers.FirstOrDefault(cm => cm.UserId == userInfo.UserId))
+                .ThenInclude(c => c.CircleMembers)
                     .ThenInclude(cm => cm!.ExceptionPermission)
             .Include(p => p.Circle)
                 .ThenInclude(c => c.DefaultPermission)
@@ -105,7 +105,8 @@ public class ProposalService(AppDBContext db, IUserInfo userInfo,
             .SingleOrDefaultAsync(p => p.Id == id, ct)
             ?? throw new InvalidDataException("Proposal not found");
 
-        validationService.CheckPermission(proposal.Circle.CircleMembers.First(), proposal.Circle, PermissionsEnum.ProposalManagement);
+        var circleMember = await validationService.TryGetCircleMemberAsync(userInfo.UserId, proposal.CircleId, ct);
+        validationService.CheckPermission(circleMember, proposal.Circle, PermissionsEnum.ProposalManagement);
 
         if (proposal.Status == ProposalStatusEnum.NoApproval)
             throw new InvalidOperationException("Proposal already not approved");
