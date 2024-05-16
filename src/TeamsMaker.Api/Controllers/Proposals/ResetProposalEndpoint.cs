94dcd938d2fd3f52618cbcc5657e58e649ca8315
Muha Saad <mu7ammadsa3dd@gmail.com>
2024-05-17 02:35:19 +0300

    [Add] add reset endpoint

---
+++ src/TeamsMaker.Api/Controllers/Proposals/ResetProposalEndpoint.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using TeamsMaker.Api.Contracts.Requests.Proposal;
+using TeamsMaker.Api.Services.Proposals.Interfaces;
+
+namespace TeamsMaker.Api.Controllers.Proposals;
+
+[Authorize]
+public class ResetProposalEndpoint(IProposalService proposalService) : BaseApiController
+{
+    [Tags("proposal")]
+    [HttpPatch("proposal/{id}/reset")]
+    public async Task<IActionResult> Reset(Guid id, CancellationToken ct)
+    {
+        try
+        {
+            await proposalService.ResetAsync(id, ct);
+        }
+        catch (Exception e)
+        {
+            return NotFound(_response.FailureResponse(e.Message));
+        }
+
+        return Ok(_response.SuccessResponse(null));
+    }
+}
