c65365fbf4f05bf8f0449d894e2828082d2f6f9e
Adham Khalid <6um6l6ee@gmail.com>
2024-05-17 20:14:58 +0300

    [Add] GetApprovalRequest By ID

---
+++ src/TeamsMaker.Api/Controllers/ApprovalRequest/GetApprovalRequestEndpoint.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using TeamsMaker.Api.Contracts.Responses.ApprovalRequest;
+using TeamsMaker.Api.Services.ApprovalRequests.Interfaces;
+
+namespace TeamsMaker.Api.Controllers.ApprovalRequest;
+
+[Authorize]
+public class GetApprovalRequestEndpoint(IApprovalRequestService approvalRequestService) : BaseApiController
+{
+    [Tags("proposals/approval_requests")]
+    [Produces<GetApprovalRequestResponse>]
+    [HttpGet("proposals/approval_requests/{id}")]
+    public async Task<IActionResult> ApprovalRequests(Guid id, CancellationToken ct)
+    {
+        GetApprovalRequestResponse approvalRequestResponse;
+
+        try
+        {
+            approvalRequestResponse = await approvalRequestService.GetAsync(id, ct);
+        }
+        catch (Exception e)
+        {
+            return NotFound(_response.FailureResponse(e.Message));
+        }
+
+        return Ok(_response.SuccessResponse(approvalRequestResponse));
+    }
+}
